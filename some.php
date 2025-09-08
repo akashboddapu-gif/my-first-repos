d91c32673e4fc1f708aa77143485fa5e4a7774f1
akashboddapu-gif <akashboddapu@gmail.com>
2025-09-08 11:35:49 +0530

    Update some.php

--- some.php
+++ some.php
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() 
 {
-    int a, b,c, sum;
+    int a, b, c, sum;
     printf("Enter two numbers: ");
-    scanf("%d %d %d", &a, &b,&c);
+    scanf("%d %d %d", &a, &b, &c);
     sum = a + b + c;
-    printf("Sum of %d and %d is %d\n", a, b,c, sum);
+    printf("Sum of %d and %d is %d\n", a, b, c, sum);
     return 0;
 
 }
+
