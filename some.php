7ceaac8b392f04e5b5f169348e112b327807f662
akashboddapu-gif <akashboddapu@gmail.com>
2025-09-08 11:31:32 +0530

    Update some.txt
    extended the addition of two numbers

--- some.php
+++ some.php
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main() 
 {
-    int a, b, sum;
+    int a, b,c, sum;
     printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-    sum = a + b;
-    printf("Sum of %d and %d is %d\n", a, b, sum);
+    scanf("%d %d %d", &a, &b,&c);
+    sum = a + b + c;
+    printf("Sum of %d and %d is %d\n", a, b,c, sum);
     return 0;
+
 }
